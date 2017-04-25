e8db069714bfd3399cde647a2d3ac691d82269d8
Ayrton Rafael Risco Torres <ayrton.risco.torres@gmail.com>
2017-04-25 14:31:44 +0200

    Agregada las peticiones GET al servidor.

--- cliente/clienteRaspberry.py
+++ cliente/clienteRaspberry.py
@@ -1,10 +1,21 @@
-#Se requiere de la instalacion de la libreria "serial"
+#Se requiere de la instalacion de la libreria "serial" y "requests"
 #El puerto actual por el que se comunica es el visualizado desde el portatil, se requiere el de la Raspi
-import serial
-ser = serial.Serial(port='/dev/cu.usbmodemFA131',baudrate=9600)
+import time
+
+import requests
+
+URL = 'http://192.168.168.197:8080/r2cam'
+# Lo siguiente funciona
+# ser = serial.Serial(port='/dev/cu.usbmodemFA131',baudrate=9600)
+
+#Introduce un caracter para empezar a hacer peticiones
 print ("Introduce un caracter (0 para salir):" )
 entrada = int(input())
-while entrada != 0:  
-   ser.write(entrada)   
-   print ("Introduce un caracter (0' para salir): ") 
-   entrada = int(input())
+while entrada != 0:
+   #ser.write(entrada)
+   r = requests.get(URL)
+   print(r.json()['direccion'])
+   #Un sleep de 0.100 segundos. El valor dentro de sleep() es en segundos
+   time.sleep(.100)
+   # print ("Introduce un caracter (0' para salir): ")
+   # entrada = int(input())
